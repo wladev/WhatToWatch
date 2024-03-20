289f50f3da6a0d63eeabde7ae63c0eaf959a7ba6
wlad <wladimir.perfiloff@gmail.com>
2024-03-20 16:25:35 +0100

    Five Star V2

--- src/components/FiveStartRating/FiveStartRating.jsx
+++ src/components/FiveStartRating/FiveStartRating.jsx
@@ -1,5 +1,29 @@
 import s from "./FiveStartRating.module.css";
+import { StarFill, Star as StartEmpty, StarHalf } from "react-bootstrap-icons";
 
-export function FiveStartRating(props) {
-    return <div></div>
+
+export function FiveStartRating({rating}) {
+    const starList = [];
+
+    const starFillCount = Math.floor(rating);
+    console.log("rating", starFillCount);
+
+    const hasStarHalf = rating - starFillCount >= 0.5
+
+    const emptyStarCount = 5 - starFillCount - (hasStarHalf ? 1 : 0);
+
+    for (let i = 1; i <= starFillCount; i++) {
+        starList.push(<StarFill key={"star-fill" + i} />)
+    }
+    if (hasStarHalf) {
+        starList.push(<StarHalf key={"star-half"} />)
+    }
+    if (emptyStarCount) {
+        starList.push(<StartEmpty key={"star-empty"} />)
+    }
+    return (
+        <div>
+          {starList}
+        </div>
+      );
 }
